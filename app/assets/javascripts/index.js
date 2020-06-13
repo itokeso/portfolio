b3e05ad8afe51b9a1d121e87eafce8504fd15f48
itokeso <61177741+itokeso@users.noreply.github.com>
2020-06-13 13:16:01 +0900

    js

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,5 +1,4 @@
 // サークルアップ
-
 $(function(){
   let topBtn = $('.fa.fa-chevron-circle-up')
   topBtn.hide();
@@ -15,5 +14,32 @@ $(function(){
       scrollTop: 0    
     }, 500);
     return false;
+  });
+});
+
+// ヘッダースクロール
+$(function(){
+  // #で始まるリンクをクリックしたら実行されます
+  $('a[href^="#"]').click(function() {
+    // スクロールの速度
+    var speed = 400; // ミリ秒で記述
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
+});
+
+$(function(){
+  $('#btn').on('click', function(){
+    $('#menu').toggleClass('active');
+    $('.black-bg').toggleClass('active');  
+    return false
+  });
+  $('.black-bg').on('click',function(){
+    $(this).removeClass('active');
+    $('#menu').removeClass('active');
+    return false
   });
 });
